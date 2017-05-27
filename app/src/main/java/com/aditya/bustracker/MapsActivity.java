978b95a80858ce3d434a6eb506d587d8328bebd1
Aditya <vns.aditya@gmail.com>
2017-05-27 16:14:24 +0530

    Prototype

--- app/src/main/java/com/aditya/bustracker/MapsActivity.java
+++ app/src/main/java/com/aditya/bustracker/MapsActivity.java
@@ -1,7 +1,9 @@
-package com.aditya.passengerbustracker;
+package com.aditya.bustracker;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,19 +12,35 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Intent i = getIntent();
+        String value = i.getStringExtra("Values");
+
+        Toast.makeText(MapsActivity.this,value,Toast.LENGTH_SHORT).show();
     }
 
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
---
+++ app/src/main/java/com/aditya/bustracker/StartBusSettingsActivity.java
@@ -0,0 +1,13 @@
+package com.aditya.bustracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartBusSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_bus_settings);
+    }
+}
